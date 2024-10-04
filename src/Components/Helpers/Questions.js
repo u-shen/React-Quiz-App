341aecd3d1b6a49601a6b0fde1181abc6136e9bc
u-shen <farah.ushen@gmail.com>
2024-10-04 08:28:26 -0700

    added reall questions

--- src/Components/Helpers/Questions.js
+++ src/Components/Helpers/Questions.js
@@ -1,24 +1,39 @@
 const question = [
   {
-    prompt: "what is my name",
-    optionA: "Ahmed",
-    optionB: "Ushen",
-    optionC: "Tailwind",
+    prompt: "What color is the sky on a clear day?",
+    optionA: "Green",
+    optionB: "Blue",
+    optionC: "Red",
     answer: "B",
   },
   {
-    prompt: "HELLO is my name",
-    optionA: "Ahmed",
-    optionB: "Ushen",
-    optionC: "Tailwind",
+    prompt: "What do you call a baby cat?",
+    optionA: "Puppy",
+    optionB: "Kitten",
+    optionC: "Foal",
     answer: "B",
   },
   {
-    prompt: "HEYY is my name",
-    optionA: "Ahmed",
-    optionB: "Ushen",
-    optionC: "Tailwind",
+    prompt: "What fruit is known for being yellow and curved?",
+    optionA: "Apple",
+    optionB: "Banana",
+    optionC: "Orange",
+    answer: "B",
+  },
+  {
+    prompt: "What sound does a cow make?",
+    optionA: "Baa",
+    optionB: "Moo",
+    optionC: "Quack",
+    answer: "B",
+  },
+  {
+    prompt: "What is 1 + 1?",
+    optionA: "1",
+    optionB: "2",
+    optionC: "3",
     answer: "B",
   },
 ];
+
 export default question;
